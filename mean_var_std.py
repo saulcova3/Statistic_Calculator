278e5c4fa05ea554aa43e425286f672b66a8c142
Saúl Cova <145939303+saulcova3@users.noreply.github.com>
2025-05-30 00:03:31 +0000

    Versión corregida

--- mean_var_std.py
+++ mean_var_std.py
@@ -6,38 +6,37 @@ def calculate(list):
     
     matrix = np.array(list).reshape(3, 3)
     
-    # Calculations for rows (axis=1), columns (axis=0), and flattened
+    # Calculations for rows (axis=0), columns (axis=1), and flattened
     calculations = {
         'mean': [
-            np.mean(matrix, axis=1).tolist(),  # Rows
-            np.mean(matrix, axis=0).tolist(),  # Columns
+            np.mean(matrix, axis=0).tolist(),  # Rows
+            np.mean(matrix, axis=1).tolist(),  # Columns
             np.mean(matrix)                   # Flattened
         ],
         'variance': [
-            np.var(matrix, axis=1).tolist(),   # Rows
-            np.var(matrix, axis=0).tolist(),   # Columns
+            np.var(matrix, axis=0).tolist(),   # Rows
+            np.var(matrix, axis=1).tolist(),   # Columns
             np.var(matrix)                     # Flattened
         ],
         'standard deviation': [
-            np.std(matrix, axis=1).tolist(),   # Rows
-            np.std(matrix, axis=0).tolist(),   # Columns
+            np.std(matrix, axis=0).tolist(),   # Rows
+            np.std(matrix, axis=1).tolist(),   # Columns
             np.std(matrix)                     # Flattened
         ],
         'max': [
-            np.max(matrix, axis=1).tolist(),   # Rows
-            np.max(matrix, axis=0).tolist(),   # Columns
+            np.max(matrix, axis=0).tolist(),   # Rows
+            np.max(matrix, axis=1).tolist(),   # Columns
             np.max(matrix)                     # Flattened
         ],
         'min': [
-            np.min(matrix, axis=1).tolist(),   # Rows
-            np.min(matrix, axis=0).tolist(),   # Columns
+            np.min(matrix, axis=0).tolist(),   # Rows
+            np.min(matrix, axis=1).tolist(),   # Columns
             np.min(matrix)                     # Flattened
         ],
         'sum': [
-            np.sum(matrix, axis=1).tolist(),   # Rows
-            np.sum(matrix, axis=0).tolist(),   # Columns
+            np.sum(matrix, axis=0).tolist(),   # Rows
+            np.sum(matrix, axis=1).tolist(),   # Columns
             np.sum(matrix)                    # Flattened
         ]
     }
-    
     return calculations
